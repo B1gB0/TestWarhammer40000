35753540fe310955874e2a27f78841bf5b8685c6
B1g_B0$$ <grand0077@inbox.ru>
2026-04-10 13:22:27 +0300

    First commit

---
+++ Assets/Project/Scripts/Game/GameRoot/Scenes.cs
@@ -0,0 +1,9 @@
+﻿namespace Project.Scripts.Game.GameRoot
+{
+    public static class Scenes
+    {
+        public const string Boot = nameof(Boot);
+        public const string MainMenu = nameof(MainMenu);
+        public const string VillageHub = nameof(VillageHub);
+    }
+}
