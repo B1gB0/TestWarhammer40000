0dd5278adc6edc4b574afacbd7d5fc9e9baf3480
B1g_B0$$ <grand0077@inbox.ru>
2026-04-11 12:51:18 +0300

    Верстка скролбара под модификаторы, добавление шрифтов, начало верстки abilities

---
+++ Assets/Project/Scripts/UI/View/AbilityView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Scripts.UI.View
+{
+    public class AbilityView : View
+    {
+        [SerializeField] private TMP_Text _nameText;
+        [SerializeField] private Image _icon;
+        [SerializeField] private Image _iconOfModification;
+        [SerializeField] private List<Sprite> _iconSpritesOfModification;
+
+        public void Set()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Project/Scripts/UI/View/ModificationView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Scripts.UI.View
+{
+    public class ModificationView : View
+    {
+        [SerializeField] private TMP_Text _nameText;
+        [SerializeField] private TMP_Text _modificationTypeName;
+        [SerializeField] private Image _icon;
+        [SerializeField] private List<Sprite> _iconSprites;
+
+        public void SetData()
+        {
+            
+        }
+    }
+}
