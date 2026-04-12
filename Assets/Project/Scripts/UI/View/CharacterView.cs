9c82a845504350cb2428586c4385ce6c182d3315
B1g_B0$$ <grand0077@inbox.ru>
2026-04-12 12:37:07 +0300

    Черновая верстка готова

--- Assets/Project/Scripts/UI/View/CharacterView.cs
+++ Assets/Project/Scripts/UI/View/CharacterView.cs
@@ -1,7 +1,14 @@
-﻿namespace Project.Scripts.UI.View
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Scripts.UI.View
 {
     public class CharacterView : View
     {
-        
+        [SerializeField] private Image _portrait;
+        [SerializeField] private TMP_Text _name;
+        [SerializeField] private TMP_Text _health;
+        [SerializeField] private TMP_Text _armor;
     }
 }
--- Assets/Project/Scripts/UI/View/PortraitView.cs
+++ Assets/Project/Scripts/UI/View/PortraitView.cs
@@ -1,7 +1,10 @@
-﻿namespace Project.Scripts.UI.View
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Scripts.UI.View
 {
     public class PortraitView : View
     {
-        
+        [SerializeField] private Image _icon;
     }
 }
